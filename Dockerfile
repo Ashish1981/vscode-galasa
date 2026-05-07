634c6804232541c84d472e8ec90c00376470d6f9
Ashish K Sahoo <ashissah@in.ibm.com>
2026-05-07 00:19:48 +0000

    First Commit

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM httpd:2.4
-
-ARG gitHash
-
-RUN rm -v /usr/local/apache2/htdocs/*
-COPY resources/httpd.conf /usr/local/apache2/conf/httpd.conf
-
-COPY galasa-plugin-*.vsix /usr/local/apache2/htdocs/
--- src/webviews/terminal/TerminalSize.ts
+++
@@ -1,9 +0,0 @@
-export class TerminalSize {
-    public columns:number;
-    public rows:number;
-
-    constructor(columns:number, rows:number) {
-        this.columns = columns;
-        this.rows = rows;
-    }
-}
